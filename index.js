f3494eb43175110989706fbd3cf45038b5129438
JosiasAurel <ndjosiasaurel@gmail.com>
2020-12-06 19:33:14 +0100

    create basic init fx

--- index.js
+++ index.js
@@ -5,5 +5,42 @@
 
 console.log("YATA")
 
+
+// initialize lowdb 
+const low = require("lowdb")
+
+const FileSync = require("lowdb/adapters/FileSync")
+const fs = require("fs")
+
+const adapter = new FileSync("todos.json")
+const db = low(adapter)
+
 // get cli args
 const cliArgs = process.argv.slice(2) // remove the first 2 default args
+
+// YATA can CRUD tasks
+
+function initApp() {
+    let isInitialized = false // variable indicating if app is initialized or not
+    try {
+        fs.readFileSync("./todos.json")
+        isInitialized = true
+    } catch (error) {
+        isInitialized = false
+    }
+
+    if (isInitialized) {
+        return
+    } else {
+        db.defaults({
+        todos: [],
+        tasksCount: 0
+    }).write()
+    }
+    return 
+}
+
+// utility functions to manage tasks
+function createTask(taskVakue) {
+
+}
