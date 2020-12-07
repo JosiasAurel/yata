b434892d369c8a3048f91f948c05476eb5e5caee
JosiasAurel <ndjosiasaurel@gmail.com>
2020-12-07 05:17:17 +0100

    migrate tests to test file2

--- index.js
+++ index.js
@@ -96,30 +96,3 @@ function showTasks() {
     return tasks
 }
 
-/* Testing the code */
-
- app() // init app
-
-console.log("Create some tasks")
-createTask("Eat some meatballs")
-createTask("Finish this app")
-createTask("You got some assignments dude")
-
-console.log("Get all tasks")
-console.log(showTasks())
-
-console.log(`Tasks count ${db.get("tasksCount").value()}`)
-
-console.log("Delete task")
-deleteTask(1)
-console.log(showTasks())
-
-console.log(`Tasks count ${db.get("tasksCount").value()}`)
-
-deleteTask(2)
-console.log(showTasks())
-
-console.log(`Tasks count ${db.get("tasksCount").value()}`)
-//console.log("Delete task 1")
-
- 
